8451c02f71ec3dd3cf5da4a82e9a9a92f916826b
marisbotero <maris2143@gmail.com>
2017-10-15 21:39:33 -0500

    fix

--- Newton-Rhapson.py
+++ Newton-Rhapson.py
@@ -13,7 +13,7 @@ def f(x):             #definir la funcion
 def Df(x):           #derivar la funcion
   return np.exp(x) -1
   
-x0=1
+x0=2
 for interacion in range(1,6):   #ciclo para recorrer el metodo de Newton R y encontrar valores aproximados a la solucion
     x1= x0 -f(x0)/Df(x0)
     x0=x1
